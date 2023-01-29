a13cb6cd14da737b901caadbe571404b556bff25
varunbasotra14 <varunbasotr14@gmail.com>
2023-01-28 23:32:17 -0500

    Committing new File Variable_Use.sh

---
+++ Scripts/Variable_Use.sh
@@ -0,0 +1,16 @@
+#Author : Varun Sharma
+#Date : 28-Jan-2023
+#Description : Use of If Command
+#!/bin/bash
+#Decalring Variable
+WORD="Hello"
+#Using command in Variable
+WORK_DIR=$(pwd)
+#Printing variable
+echo "${WORD} Varun"
+#Prinitng variable using alternative way
+echo "$WORD Varun"
+#Prinitng varibale stroing command
+echo "You are in ${WORK_DIR}"
+#Printin variable using single quotes
+echo '$WORD Varun'
